7710131496b6a92d79b95d3c08c1aa61205112e5
Romane G <romane71193@gmail.com>
2019-06-29 17:33:18 -0400

    typecript and container components

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -20,7 +20,7 @@ interface IAppState {
 
 class App extends React.Component<IAppProps, IAppState> {
   //setState: any;
-  constructor(props) {
+  constructor(props: IAppProps) {
     super(props)
     this.state = {
       robots: [],
@@ -28,17 +28,17 @@ class App extends React.Component<IAppProps, IAppState> {
     }
   }
 
-  componentDidMount() {
+  componentDidMount():void {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response=> response.json())
       .then(users => {this.setState({ robots: users})});
   }
 
-  onSearchChange = (event) => {
+  onSearchChange = (event: React.SyntheticEvent<HTMLInputElement>):void => {
     this.setState({ searchfield: event.currentTarget.value })
   }
 
-  render() {
+  render():JSX.Element {
     const { robots, searchfield } = this.state;
     const filteredRobots = robots.filter(robot =>{
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
